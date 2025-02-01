b8db506bd0fa999fb537e33f2aa7851b784f0435
Dave Mcsavvy <davemcsavvii@gmail.com>
2025-02-01 02:02:22 +0100

    Add tests for royalties and batch minting functionality

--- tests/pix_mint_test.ts
+++ tests/pix_mint_test.ts
@@ -19,15 +19,36 @@ Clarinet.test({
       ], wallet1.address)
     ]);
     
-    block.receipts[0].result.expectOk();
+    block.receipts[0].result.expectOk().expectUint(1);
     block.receipts[0].events.expectNonFungibleTokenMint('pix-nft', types.uint(1), wallet1.address);
   },
 });
 
 Clarinet.test({
-  name: "Can transfer NFT between accounts",
+  name: "Can batch mint multiple NFTs",
   async fn(chain: Chain, accounts: Map<string, Account>) {
-    const deployer = accounts.get('deployer')!;
+    const wallet1 = accounts.get('wallet_1')!;
+    
+    const uris = [
+      "ipfs://Qm1",
+      "ipfs://Qm2",
+      "ipfs://Qm3"
+    ];
+    
+    let block = chain.mineBlock([
+      Tx.contractCall('pix-mint', 'batch-mint', [
+        types.list(uris.map(uri => types.ascii(uri)))
+      ], wallet1.address)
+    ]);
+    
+    block.receipts[0].result.expectOk().expectUint(1);
+    assertEquals(block.receipts[0].events.length, 3);
+  },
+});
+
+Clarinet.test({
+  name: "Can set and collect royalties",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
     const wallet1 = accounts.get('wallet_1')!;
     const wallet2 = accounts.get('wallet_2')!;
     
@@ -38,7 +59,17 @@ Clarinet.test({
       ], wallet1.address)
     ]);
     
-    // Then transfer
+    // Set custom royalty
+    let royaltyBlock = chain.mineBlock([
+      Tx.contractCall('pix-mint', 'set-token-royalty', [
+        types.uint(1),
+        types.uint(10)
+      ], wallet1.address)
+    ]);
+    
+    royaltyBlock.receipts[0].result.expectOk();
+    
+    // Transfer will trigger royalty payment
     let transferBlock = chain.mineBlock([
       Tx.contractCall('pix-mint', 'transfer', [
         types.uint(1),
@@ -48,17 +79,16 @@ Clarinet.test({
     ]);
     
     transferBlock.receipts[0].result.expectOk();
-    transferBlock.receipts[0].events.expectNonFungibleTokenTransfer(
-      'pix-nft',
-      types.uint(1),
-      wallet1.address,
-      wallet2.address
+    transferBlock.receipts[0].events.expectSTXTransferEvent(
+      1000000, // 10% of mint price
+      wallet2.address,
+      wallet1.address
     );
   },
 });
 
 Clarinet.test({
-  name: "Can retrieve token metadata",
+  name: "Can retrieve token metadata and creator",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     const wallet1 = accounts.get('wallet_1')!;
     const metadataUri = "ipfs://QmExample";
@@ -71,12 +101,27 @@ Clarinet.test({
     ]);
     
     // Get metadata
-    let block = chain.mineBlock([
+    let metadataBlock = chain.mineBlock([
       Tx.contractCall('pix-mint', 'get-token-uri', [
         types.uint(1)
       ], wallet1.address)
     ]);
     
-    block.receipts[0].result.expectSome().expectAscii(metadataUri);
+    metadataBlock.receipts[0].result
+      .expectOk()
+      .expectSome()
+      .expectAscii(metadataUri);
+      
+    // Get creator
+    let creatorBlock = chain.mineBlock([
+      Tx.contractCall('pix-mint', 'get-token-creator', [
+        types.uint(1)
+      ], wallet1.address)
+    ]);
+    
+    creatorBlock.receipts[0].result
+      .expectOk()
+      .expectSome()
+      .expectPrincipal(wallet1.address);
   },
 });
